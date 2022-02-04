a27f035615770a19a7089eb2de895fc99dabcbe0
yglee8048 <yglee8048@gmail.com>
2022-02-04 18:40:39 +0900

    first

---
+++ src/test/java/com/lgcns/icst/mission/spring/jsp/hangma/HangmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.icst.mission.spring.jsp.hangma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
